6f96403d911d6b27097493b28d9c0780697cd4fa
matovu-farid <matovu90@gmail.com>
2022-01-26 16:05:34 +0300

    Configure webpack for use of async and await

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import './style.css';
+import './leaderboard_api'
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,11 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
     ],
   },
   devtool: 'inline-source-map',
